7ff95cb131f71122005652d6dcde15ee64d6571f
Samba Siva Rao Kolusu <saasira@gmail.com>
2025-07-16 12:21:43 +0530

    fixed a compiling issue in copy-link.js

--- theme/src/components/copy-link/copy-link.js
+++ theme/src/components/copy-link/copy-link.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	if(!document.body.classList.contains("fsi-guides-topic-page")) {
 		return;
 	}
-    $("#gu-copy-link-fsi")?.on("click", function () {
+    $("#gu-copy-link-fsi").on("click", function () {
         // Show the copy confirmation
         $("#link-fsi").fadeIn(200).delay(1000).fadeOut(200);
 
@@ -13,7 +13,7 @@ $(document).ready(function () {
 	if(!document.body.classList.contains("fsi-guides-topic-page")) {
 		return;
 	}
-    $("#gu-copy-link-fsi-mobile")?.on("click", function () {
+    $("#gu-copy-link-fsi-mobile").on("click", function () {
         // Show the copy confirmation
         $("#link-fsi-mobile").fadeIn(200).delay(1000).fadeOut(200);
 
@@ -26,7 +26,7 @@ $(document).ready(function () {
 	if(!document.body.classList.contains("manufacturing-guides-topic-page")) {
 		return;
 	}
-    $("#gu-copy-link-fsi")?.on("click", function () {
+    $("#gu-copy-link-fsi").on("click", function () {
         // Show the copy confirmation
         $("#link-fsi").fadeIn(200).delay(1000).fadeOut(200);
 
@@ -37,7 +37,7 @@ $(document).ready(function () {
 	if(!document.body.classList.contains("manufacturing-guides-topic-page")) {
 		return;
 	}
-    $("#gu-copy-link-fsi-mobile")?.on("click", function () {
+    $("#gu-copy-link-fsi-mobile").on("click", function () {
         // Show the copy confirmation
         $("#link-fsi-mobile").fadeIn(200).delay(1000).fadeOut(200);
 
